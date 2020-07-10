15128bcb66f3212b2f537e86f00d98fd67610a17
TD <872926091@qq.com>
2020-07-10 14:12:18 +0800

    ver1.1 还没做完

---
+++ app/src/main/java/com/example/shoptest2/RegisterActivity.java
@@ -0,0 +1,48 @@
+package com.example.shoptest2;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView register;
+    private AlertDialog.Builder builder;
+    private String a="2900";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+        register=findViewById(R.id.register);
+        register.setOnClickListener(this);
+
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        builder = new AlertDialog.Builder(this).setIcon(R.mipmap.ic_launcher).setTitle("注册成功")
+                .setMessage("您的账号为："+a+"         请牢记").setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //ToDo: 你想做的事情
+                        Toast.makeText(RegisterActivity.this, "注册成功", Toast.LENGTH_LONG).show();
+                    }
+                }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //ToDo: 你想做的事情
+                       // Toast.makeText(RegisterActivity.this, "关闭按钮", Toast.LENGTH_LONG).show();
+                        dialogInterface.dismiss();
+                    }
+                });
+        builder.create().show();
+
+    }
+}
