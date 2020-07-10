4bd5689eecac16bd798c26d3f24599041c5ecb8a
bilongtang <1032576433@qq.com>
2020-07-10 22:37:51 +0800

    Merge branch 'master' of D:\AndroidStudioProjects\shoptest2 with conflicts.

---
+++ app/src/main/java/com/example/shoptest2/LoginActivity.java
@@ -0,0 +1,142 @@
+package com.example.shoptest2;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.TypeReference;
+import com.example.shoptest2.entity.UserEntity;
+import com.example.shoptest2.util.Constant;
+import com.example.shoptest2.util.HttpHelper;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import okhttp3.Call;
+import okhttp3.Response;
+
+public class LoginActivity  extends AppCompatActivity  implements View.OnClickListener {
+    private EditText userId,passWord;
+    private TextView modifyPassword,forgetPassword,login,register;
+
+    private final Context context=this;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        init();
+    }
+    private void init(){
+        userId=findViewById(R.id.userId);
+        passWord=findViewById(R.id.passWord);
+
+        forgetPassword=findViewById(R.id.forgetPassword);
+        login=findViewById(R.id.login);
+        register=findViewById(R.id.register);
+     //   userId.setOnClickListener(this);
+      //  passWord.setOnClickListener(this);
+        modifyPassword.setOnClickListener(this);
+        forgetPassword.setOnClickListener(this);
+        login.setOnClickListener(this);
+        register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+
+            case  R.id.forgetPassword:
+
+                break;
+            case R.id.login:
+                final HttpHelper instance=HttpHelper.getInstance();
+                UserEntity user=new UserEntity();
+                user.setUserId(userId.getText().toString());
+                user.setPassWord(passWord.getText().toString());
+                instance.postDataAsyn(Constant.parentUrl+"login", JSON.toJSONString(user), new HttpHelper.NetCall(){
+
+
+                    @Override
+                    public void success(Call call, final Response response) throws IOException {
+
+                        runOnUiThread(new  Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    HashMap<String,Object> msg= JSONObject.parseObject(response.body().string(), new TypeReference<HashMap<String, Object>>(){});
+                                   if(!(Boolean) msg.get("flag")) {
+                                       Toast.makeText(context,(String) msg.get("msg"), Toast.LENGTH_SHORT).show();
+                                   }else
+                                   {
+                                       Intent intent=new Intent(context,MainActivity.class);
+                                       instance.setToken((String)msg.get("token"));
+                                       startActivity(intent);
+                                   }
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                    Toast.makeText(context,"服务器请求错误", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void failed(Call call, final IOException e) {
+
+                        runOnUiThread(new  Runnable() {
+
+                            @Override
+                            public void run() {
+                                Toast.makeText(context,"服务器请求失败",Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
+                    }
+                });
+                break;
+            case  R.id.register:
+                Intent intent =new Intent(context,RegisterActivity.class);
+                startActivityForResult(intent,Constant.registerCode);
+                break;
+        }
+
+    }
+    @Override
+    protected  void  onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bundle bundle=data.getBundleExtra("msg");
+        switch (requestCode){
+            case Constant.registerCode:
+                if(resultCode==Constant.result_success)
+                {
+                    userId.setText(bundle.getString("userid"));
+                }else
+                {
+                    Toast.makeText(context,"注册失败", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case Constant.forgetPasswordCode:
+                if(requestCode==Constant.result_success)
+                {
+
+                }else
+                {
+                    Toast.makeText(context,"注册失败", Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/shoptest2/util/Constant.java
@@ -0,0 +1,23 @@
+package com.example.shoptest2.util;
+
+import okhttp3.MediaType;
+
+public final class Constant {
+    /**
+     * url*/
+    public static  final  String parentUrl="http://192.168.1.106:8080/";
+    /**
+     * 结果码，成功*/
+    public static final  int result_success=1;
+
+    /**
+     * 结果码 ,失败*/
+    public  static final  int result_failed=2;
+    /**
+     * 忘记密码请求吗*/
+    public final static int forgetPasswordCode=1;
+    /**
+     * 注册密码请求吗*/
+    public final static int  registerCode=2;
+
+}
